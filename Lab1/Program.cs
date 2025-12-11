c3ac26f9954fbee45db63f117793a80ddadc7d5d
Dukukkb <debanelney@gmail.com>
2025-12-12 02:01:08 +0300

    Изменил структуру лабы

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -20,32 +20,32 @@ class Program
         manager.AddTeacher(teacher1);
         manager.AddTeacher(teacher2);
 
-        var onlineCourse = new OnlineCourse(
+        var onlineCourse1 = new OnlineCourse(
             id: null,
             name: "Объектно ориентированное программирование",
             maxStudents: 25,
             platform: "Zoom"
         );
 
-        var offlineCourse = new OfflineCourse(
+        var offlineCourse1 = new OfflineCourse(
             id: null,
             name: "Объектно ориентированное программирование",
             maxStudents: 20,
             classroom: "Аудитория 305"
         );
 
-        manager.AddCourse(onlineCourse);
-        manager.AddCourse(offlineCourse);
+        manager.AddCourse(onlineCourse1);
+        manager.AddCourse(offlineCourse1);
 
-        manager.AssignTeacher(onlineCourse.Id, teacher1.Id);
-        manager.AssignTeacher(offlineCourse.Id, teacher2.Id);
+        manager.AssignTeacher(onlineCourse1.Id, teacher1.Id);
+        manager.AssignTeacher(offlineCourse1.Id, teacher2.Id);
 
         var student1 = new Student("K3239", " Бандурин Егор Сергеевич", "egor@mail.ru");
         var student2 = new Student("K3242", "Веселков Матвей Евгеньевич", "matvey@mail.ru");
 
-        manager.EnrollStudent(onlineCourse.Id, student1);
-        manager.EnrollStudent(onlineCourse.Id, student2);
-        manager.EnrollStudent(offlineCourse.Id, student1);
+        manager.EnrollStudent(onlineCourse1.Id, student1);
+        manager.EnrollStudent(onlineCourse1.Id, student2);
+        manager.EnrollStudent(offlineCourse1.Id, student1);
 
         Console.WriteLine("Все курсы:");
         foreach (var course in manager.GetAllCourses())
